d9ab9f4f462dbf236e10b8d264b31847d493592c
rakun256 <emreuslu256@gmail.com>
2025-03-20 00:29:18 +0300

    Addon: High Score made savable in data.txt.

--- scoreboard.py
+++ scoreboard.py
@@ -6,12 +6,15 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(-310, 310)
         self.hideturtle()
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("#3C412C")
 
     def show_board(self, score):
         super().clear()
         if score > self.high_score:
             self.high_score = score
+            with open("data.txt", "w") as data:
+                data.write(str(self.high_score))
         message = f"Your current score is: {score}, Your high score is: {self.high_score}"
         super().write(align="left", move=False, arg=message, font=("Courier", 12, "bold"))
